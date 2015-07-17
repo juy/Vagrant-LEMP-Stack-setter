c21c46e512647d32c4bc728d4be7a5f29baadb5f
angelside <angelside@users.sourceforge.net>
2015-07-17 23:31:27 +0300

    Updates

--- vagrant/provision/provision.sh
+++ vagrant/provision/provision.sh
@@ -7,21 +7,21 @@ ANSIBLE_PLAYBOOK=$3
 
 # Check if ansible installed
 if ! which ansible > /dev/null; then
-	# Ansible does't exist, install it
-	echo "Installing ansible";
+    # Ansible does't exist, install it
+    echo "Installing ansible";
 
-	# Ansible 1.7.2
-	sudo apt-add-repository ppa:ansible/ansible
-	sudo apt-get install -y software-properties-common
-	sudo apt-get update
-	sudo apt-get install -y ansible
+    # Ansible 1.7.2
+    sudo apt-get update -qq
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get install -y -qq software-properties-common
+    sudo apt-get install -y -qq ansible
 
-	# Ansible 1.9.2
-	#sudo apt-get update -qq
-	#sudo apt-get install -y -qq software-properties-common
-	#sudo apt-get install -y -qq python-pip python-dev
-	#sudo pip -q install ansible
-	#mkdir /etc/ansible/
+    # Ansible 1.9.2
+    #sudo apt-get update -qq
+    #sudo apt-get install -y -qq software-properties-common
+    #sudo apt-get install -y -qq python-pip python-dev
+    #sudo pip -q install ansible
+    #mkdir /etc/ansible/
 fi
 
 # Ansible provision
@@ -34,9 +34,9 @@ echo "Ansible provision start..."
 
 # Run the playbook
 if [ "$VERBOSE" = "y" ]; then
-	sudo -i ansible-playbook $ANSIBLE_PLAYBOOK --connection=local -v
-	#ansible-playbook -i 'localhost,' "${ANSIBLE_PLAYBOOK}" --extra-vars "is_windows=true" --connection=local -v
+    sudo -i ansible-playbook $ANSIBLE_PLAYBOOK --connection=local -v
+    #ansible-playbook -i 'localhost,' "${ANSIBLE_PLAYBOOK}" --extra-vars "is_windows=true" --connection=local -v
 else
-	sudo -i ansible-playbook $ANSIBLE_PLAYBOOK --connection=local
-	#ansible-playbook -i 'localhost,' "${ANSIBLE_PLAYBOOK}" --extra-vars "is_windows=true" --connection=local
+    sudo -i ansible-playbook $ANSIBLE_PLAYBOOK --connection=local
+    #ansible-playbook -i 'localhost,' "${ANSIBLE_PLAYBOOK}" --extra-vars "is_windows=true" --connection=local
 fi
