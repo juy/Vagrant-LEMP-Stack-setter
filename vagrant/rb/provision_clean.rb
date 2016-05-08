71fd0f8a7ab26071fe4fa3d728c557e2d3909b8d
juysoft <sevdin.filiz@gmail.com>
2016-05-08 14:36:55 +0300

    Update

--- vagrant/rb/provision_clean.rb
+++ vagrant/rb/provision_clean.rb
@@ -1,6 +1,6 @@
 # Shell provision for box cleaning
 class Provision
-    def self.clean(config, settings)
+    def self.clean(config)
         config.vm.provision "shell", path: "./vagrant/provision/script/vagrant-clean/01-update.sh"
         config.vm.provision "shell", path: "./vagrant/provision/script/vagrant-clean/02-minimize.sh"
         config.vm.provision "shell", path: "./vagrant/provision/script/vagrant-clean/03-cleanup.sh"
